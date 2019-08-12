5865374ed9bb4f2e44fb59351eb931a6fe4d4577
Andre <andre.lipke@gmail.com>
2019-08-11 23:17:48 -0400

    Small refactor, add blog posts

---
+++ web/blog.dart
@@ -0,0 +1,179 @@
+import 'dart:async';
+import 'dart:html';
+import 'package:xml/xml.dart' as xml;
+
+Future<String> _timeoutRequest(String url,  {
+  String method,
+  bool withCredentials,
+  String responseType,
+  String mimeType,
+  Map<String, String> requestHeaders,
+  sendData,
+  void onProgress(ProgressEvent e),
+  double timeout,
+}) {
+  var completer = Completer<HttpRequest>();
+
+  var xhr = HttpRequest();
+  if (method == null) {
+    method = 'GET';
+  }
+
+  xhr.open(method, url, async: true);
+
+  if (withCredentials != null) {
+    xhr.withCredentials = withCredentials;
+  }
+
+  if (responseType != null) {
+    xhr.responseType = responseType;
+  }
+
+  if (mimeType != null) {
+    xhr.overrideMimeType(mimeType);
+  }
+
+  if (requestHeaders != null) {
+    requestHeaders.forEach((header, value) {
+      xhr.setRequestHeader(header, value);
+    });
+  }
+
+  if (onProgress != null) {
+    xhr.onProgress.listen(onProgress);
+  }
+
+  if (timeout != null) xhr.timeout = (timeout * 1000).floor();
+
+  xhr.onLoad.listen((e) {
+    var accepted = xhr.status >= 200 && xhr.status < 300;
+    var fileUri = xhr.status == 0;
+    var notModified = xhr.status == 304;
+    var unknownRedirect = xhr.status > 307 && xhr.status < 400;
+    if (accepted || fileUri || notModified || unknownRedirect) {
+      completer.complete(xhr);
+    } else {
+      completer.completeError(e);
+    }
+  });
+
+  xhr.onError.listen(completer.completeError);
+
+  if (sendData != null) {
+    xhr.send(sendData);
+  } else {
+    xhr.send();
+  }
+
+  return completer.future.then((e) => e.response);
+}
+
+void startBlog() async {
+  try {
+    var res = await _timeoutRequest("https://blog.tst.sh/rss/", timeout: 5.0);
+    var doc = xml.parse(res);
+
+    xml.XmlElement find(xml.XmlNode p, String name) {
+      for (var c in p.children) {
+        if (c is xml.XmlElement && c.name.toString() == name) return c;
+      }
+      return null;
+    }
+
+    Iterable<xml.XmlElement> findAll(xml.XmlNode p, String name) sync* {
+      for (var c in p.children) {
+        if (c is xml.XmlElement && c.name.toString() == name) yield c;
+      }
+    }
+
+    String cdata(xml.XmlNode node) => (node.firstChild as xml.XmlCDATA).text;
+
+    var channel = find(find(doc.root, "rss"), "channel");
+    var posts = findAll(channel, "item");
+
+    var postsContent = querySelector("#recent-posts-content");
+    //postsContent.children.clear();
+
+    var postBgs = <DivElement>[];
+    var postElms = <DivElement>[];
+    String transTo;
+    Timer transTimer;
+
+    var bgMainDiv = querySelector("#blog-posts-bg-main") as DivElement;
+    var bgMain = (querySelector("#blog-posts-bg-main").querySelector(".blog-posts-bg-inner") as DivElement);
+    var bgBack = (querySelector("#blog-posts-bg-back").querySelector(".blog-posts-bg-inner") as DivElement);
+
+    int i = 0;
+    for (var post in posts.take(6)) {
+      var title = cdata(find(post, "title"));
+      var categories = findAll(post, "category").map(cdata).toSet();
+      var banner = find(post, "media:content").attributes.firstWhere((e) => e.name.toString() == "url").value;
+
+      print(title);
+      print(categories);
+
+      var c = DivElement()
+        ..classes.add("blog-post")
+        ..children.addAll([
+          DivElement()
+            ..classes.add("blog-post-pin"),
+          DivElement()
+            ..classes.add("blog-post-title")
+            ..text = title,
+          DivElement()
+            ..classes.add("blog-post-text"),
+        ]);
+
+      c.onMouseEnter.listen((e) {
+        print("enter");
+
+        if (transTo != null) {
+          transTimer.cancel();
+          bgBack.style.backgroundImage = 'url("$transTo")';
+        }
+
+        bgMain.style.backgroundImage = 'url("$banner")';
+        bgMainDiv.style.opacity = "1";
+        bgMainDiv.style.transition = "transition: all 0.5s ease;";
+        transTo = banner;
+        transTimer = Timer(Duration(milliseconds: 500), () {
+          bgMainDiv.style.opacity = "0";
+          bgBack.style.backgroundImage = "url($transTo)";
+          transTo = null;
+        });
+
+        for (var p in postElms) if (p != c) p.style.opacity = "0.5";
+        for (var p in postBgs) p.style.opacity = "0";
+      });
+
+      c.onMouseLeave.listen((e) {
+        print("leave");
+        for (var p in postElms) p.style.opacity = "1";
+        for (var p in postBgs) p.style.opacity = "1";
+      });
+
+      postElms.add(c);
+      postsContent.children.add(c);
+
+      var d = DivElement()
+        ..classes.add("blog-posts-bg")
+        ..style.clipPath = "polygon(0 ${i * 56}px, 100% ${i * 56}px, 100% ${(i + 1) * 56}px, 0 ${(i + 1) * 56}px)"
+        ..style.zIndex = "-1"
+        ..children.add(DivElement()
+          ..classes.add("blog-posts-bg-inner")
+          ..style.backgroundImage='url("${banner}")'
+        );
+
+      postBgs.add(d);
+      postsContent.children.add(d);
+
+      i++;
+    }
+
+    querySelector("#recent-posts-content").style.opacity = "1";
+  } catch (e, bt) {
+    print("Error requesting rss feed");
+    print(e);
+    print(bt);
+  }
+}
---
+++ web/layout.dart
@@ -0,0 +1,403 @@
+import 'dart:html';
+import 'dart:math';
+
+import 'dart:svg';
+
+void startLayout() {
+  var body = querySelector("body");
+  var content = querySelector("#content");
+  var header = querySelector("#header");
+  var headerBg = querySelector("#header-bg");
+  var tstText = querySelector("#tst-text");
+  var tstBtnRow = querySelector("#tst-btn-row");
+  var aboutRow = querySelector("#about-row");
+  var about = querySelector("#about");
+  var aboutBg = querySelector("#about-bg");
+  var aboutTitle = querySelector("#about-title");
+  var aboutContent = querySelector("#about-content");
+  var links = querySelector("#links");
+  var linksBg = querySelector("#links-bg");
+  var linksTitle = querySelector("#links-title");
+  var linksContent = querySelector("#links-content");
+  var posts = querySelector("#recent-posts");
+  var postsBg = querySelector("#recent-posts-bg");
+  var postsTitle = querySelector("#recent-posts-title");
+  var postsContent = querySelector("#recent-posts-content");
+
+  void poly(Element parent, List<num> points, {String style, String transform, String opacity}) {
+    var p = PolygonElement();
+    var pts = StringBuffer();
+    for (int i = 0; i < points.length; i += 2) {
+      pts.write(points[i]);
+      pts.write(",");
+      pts.write(points[i + 1]);
+      pts.write(" ");
+    }
+    p.attributes["points"] = pts.toString();
+    if (style != null) p.attributes["style"] = style;
+    if (transform != null) p.attributes["transform"] = transform;
+    if (opacity != null) p.attributes["opacity"] = opacity;
+    parent.children.add(p);
+  }
+
+  void renderHeader() {
+    print("hello");
+    var e = SvgSvgElement();
+
+    var defs = SvgElement.tag("defs");
+
+    defs.children.add(
+        SvgElement.tag("linearGradient")
+          ..attributes["id"] = "blue"
+          ..attributes["x1"] = "0%"
+          ..attributes["x2"] = "0%"
+          ..attributes["x2"] = "100%"
+          ..attributes["y2"] = "100%"
+          ..children.addAll([
+            SvgElement.tag("stop")
+              ..attributes["offset"] = "0%"
+              ..style.setProperty("stop-color", "#2a70b3")
+              ..style.setProperty("stop-opacity", "1"),
+
+            SvgElement.tag("stop")
+              ..attributes["offset"] = "100%"
+              ..style.setProperty("stop-color", "#2d8dd7")
+              ..style.setProperty("stop-opacity", "1"),
+          ])
+    );
+
+    e.children.add(defs);
+
+    {
+      var w = header.clientWidth;
+      var h = header.clientHeight;
+      poly(e, [
+        0, 8,
+        8, 0,
+        w - 8, 0,
+        w, 8,
+        w, h - 6 * 8,
+        w - 8, h - 5 * 8,
+        w - 31 * 8, h - 5 * 8,
+        w - 36 * 8, h,
+        36 * 8, h,
+        31 * 8, h - 5 * 8,
+        8, h - 5 * 8,
+        0, h - 6 * 8,
+      ], style: "fill:#2b3d52");
+
+      poly(e, [
+        0, 8,
+        8, 0,
+        w - 8, 0,
+        w, 8,
+        w, h - 7 * 8,
+        w - 8, h - 6 * 8,
+        w - 31 * 8, h - 6 * 8,
+        w - 36 * 8, h - 8,
+        36 * 8, h - 8,
+        31 * 8, h - 6 * 8,
+        8, h - 6 * 8,
+        0, h - 7 * 8,
+      ], style: "fill:#3b536d");
+    }
+
+    {
+      var w = tstText.clientWidth;
+      var h = tstText.clientHeight;
+      poly(e, [
+        0, 8,
+        8, 0,
+        w - 8, 0,
+        w, 8,
+        w, h - 8,
+        w - 8, h,
+        8, h,
+        0, h - 8,
+      ], style: "fill:url(#blue)", transform: "translate(${tstText.offsetLeft},${tstText.offsetTop})");
+    }
+
+    var btns = querySelectorAll("#tst-btn-row .tst-btn");
+    const st = 8 / 6;
+
+    {
+      List<num> head(int ox, int oy) => [
+        18 * st + ox, oy + 3 * st,
+        16 * st + ox, oy + st,
+        16 * st + ox, oy,
+        26 * st + ox, oy,
+        26 * st + ox, oy + st,
+        24 * st + ox, oy + 3 * st,
+      ];
+
+      var w = header.clientWidth;
+      var h = header.clientHeight;
+
+      var os = btns[0].offsetTo(header);
+      poly(e, [
+        ...head(os.x, os.y + 56),
+        24 * st + os.x, h + 8 * -5 + st * -2,
+        26 * st + os.x, h + 8 * -5,
+        os.x + st * 20 + 8 * 18, h - 8 * 5,
+        os.x + st * 24 + 8 * 18, h + 8 * -5 + st * 4,
+        os.x + st * 24 + 8 * 18, h - 8,
+        os.x + st * 18 + 8 * 18, h - 8,
+        os.x + st * 18 + 8 * 18, h + 8 * -4 + st * 2,
+        os.x + st * 16 + 8 * 18, h - 8 * 4,
+        22 * st + os.x, h + 8 * -4,
+        18 * st + os.x, h + 8 * -4 + st * -4,
+      ], style: "fill:#fff", opacity: "0.27");
+
+      os = btns[1].offsetTo(header);
+      poly(e, [
+        ...head(os.x, os.y + 56),
+        24 * st + os.x, h + 8 * -7 + st * -2,
+        26 * st + os.x, h + 8 * -7,
+        os.x + st * 20 + 8 * 9, h - 8 * 7,
+        os.x + st * 24 + 8 * 9, h + 8 * -7 + st * 4,
+        os.x + st * 24 + 8 * 9, h - 8,
+        os.x + st * 18 + 8 * 9, h - 8,
+        os.x + st * 18 + 8 * 9, h + 8 * -6 + st * 2,
+        os.x + st * 16 + 8 * 9, h - 8 * 6,
+        22 * st + os.x, h + 8 * -6,
+        18 * st + os.x, h + 8 * -6 + st * -4,
+      ], style: "fill:#fff", opacity: "0.27");
+
+      os = btns[2].offsetTo(header);
+      poly(e, [
+        ...head(os.x, os.y + 56),
+        os.x + st * 24, h - 8,
+        os.x + st * 18, h - 8,
+      ], style: "fill:#fff", opacity: "0.27");
+
+      os = btns[3].offsetTo(header);
+      poly(e, [
+        ...head(os.x, os.y + 56),
+        24 * st + os.x, h + 8 * -6 + st * -4,
+        20 * st + os.x, h + 8 * -6,
+        os.x + st * 26 - 8 * 9, h - 8 * 6,
+        os.x + st * 24 - 8 * 9, h + 8 * -6 + st * 2,
+        os.x + st * 24 - 8 * 9, h - 8,
+        os.x + st * 18 - 8 * 9, h - 8,
+        os.x + st * 18 - 8 * 9, h + 8 * -7 + st * 4,
+        os.x + st * 22 - 8 * 9, h - 8 * 7,
+        16 * st + os.x, h - 8 * 7,
+        18 * st + os.x, h - 8 * 7 + st * -2,
+      ], style: "fill:#fff", opacity: "0.27");
+
+      os = btns[4].offsetTo(header);
+      poly(e, [
+        ...head(os.x, os.y + 56),
+        24 * st + os.x, h + 8 * -4 + st * -4,
+        20 * st + os.x, h + 8 * -4,
+        os.x + st * 26 - 8 * 18, h - 8 * 4,
+        os.x + st * 24 - 8 * 18, h + 8 * -4 + st * 2,
+        os.x + st * 24 - 8 * 18, h - 8,
+        os.x + st * 18 - 8 * 18, h - 8,
+        os.x + st * 18 - 8 * 18, h + 8 * -5 + st * 4,
+        os.x + st * 22 - 8 * 18, h - 8 * 5,
+        16 * st + os.x, h - 8 * 5,
+        18 * st + os.x, h - 8 * 5 + st * -2,
+      ], style: "fill:#fff", opacity: "0.27");
+    }
+
+    headerBg.children = [e];
+  }
+
+  void renderAbout() {
+    var e = SvgSvgElement();
+
+    {
+      var w = about.clientWidth;
+      var h = about.clientHeight;
+      poly(e, [
+        0, 8,
+        8, 0,
+        30 * 8, 0,
+        35 * 8, 8 * 5,
+        w - 8, 8 * 5,
+        w, 8 * 6,
+        w, h - 8,
+        w - 8, h,
+        8, h,
+        0, h - 8,
+      ], style: "fill:#2b3d52");
+    }
+
+    {
+      var w = about.clientWidth;
+      var h = about.clientHeight;
+      poly(e, [
+        0, 8,
+        8, 0,
+        30 * 8, 0,
+        35 * 8, 8 * 5,
+        w - 8, 8 * 5,
+        w, 8 * 6,
+        w, h - 16,
+        w - 8, h - 8,
+        8, h - 8,
+        0, h - 16,
+      ], style: "fill:#3b536d");
+    }
+
+    {
+      var w = aboutContent.clientWidth;
+      var h = aboutContent.clientHeight;
+      poly(e, [
+        0, 8,
+        8, 0,
+        w - 8, 0,
+        w, 8,
+        w, h - 8,
+        w - 8, h,
+        8, h,
+        0, h - 8,
+      ], style: "fill:#303d4f", transform: "translate(${aboutContent.offsetLeft},${aboutContent.offsetTop})");
+    }
+
+    aboutBg.children = [e];
+  }
+
+  void renderLinks() {
+    var e = SvgSvgElement();
+
+    {
+      var w = links.clientWidth;
+      var h = links.clientHeight;
+      poly(e, [
+        0, 8 * 6,
+        8, 8 * 5,
+        w - 35 * 8, 8 * 5,
+        w - 30 * 8, 0,
+        w - 8, 0,
+        w, 8,
+        w, h - 8,
+        w - 8, h,
+        8, h,
+        0, h - 8,
+      ], style: "fill:#2b3d52");
+    }
+
+    {
+      var w = links.clientWidth;
+      var h = links.clientHeight;
+      poly(e, [
+        0, 8 * 6,
+        8, 8 * 5,
+        w - 35 * 8, 8 * 5,
+        w - 30 * 8, 0,
+        w - 8, 0,
+        w, 8,
+        w, h - 16,
+        w - 8, h - 8,
+        8, h - 8,
+        0, h - 16,
+      ], style: "fill:#3b536d");
+    }
+
+    {
+      var w = linksContent.clientWidth;
+      var h = linksContent.clientHeight;
+      poly(e, [
+        0, 8,
+        8, 0,
+        w - 8, 0,
+        w, 8,
+        w, h - 8,
+        w - 8, h,
+        8, h,
+        0, h - 8,
+      ], style: "fill:#303d4f", transform: "translate(${linksContent.offsetLeft},${linksContent.offsetTop})");
+    }
+
+    linksBg.children = [e];
+  }
+
+  void renderPosts() {
+    var e = SvgSvgElement();
+
+    {
+      var w = posts.clientWidth;
+      var h = posts.clientHeight;
+      poly(e, [
+        0, 8,
+        8, 0,
+        30 * 8, 0,
+        35 * 8, 8 * 5,
+        w - 8, 8 * 5,
+        w, 8 * 6,
+        w, h - 8,
+        w - 8, h,
+        8, h,
+        0, h - 8,
+      ], style: "fill:#2b3d52");
+    }
+
+    {
+      var w = posts.clientWidth;
+      var h = posts.clientHeight;
+      poly(e, [
+        0, 8,
+        8, 0,
+        30 * 8, 0,
+        35 * 8, 8 * 5,
+        w - 8, 8 * 5,
+        w, 8 * 6,
+        w, h - 16,
+        w - 8, h - 8,
+        8, h - 8,
+        0, h - 16,
+      ], style: "fill:#3b536d");
+    }
+
+    /*{
+      var w = postsContent.clientWidth;
+      var h = postsContent.clientHeight;
+      poly(e, [
+        0, 8,
+        8, 0,
+        w - 8, 0,
+        w, 8,
+        w, h - 8,
+        w - 8, h,
+        8, h,
+        0, h - 8,
+      ], style: "fill:#303d4f", transform: "translate(${postsContent.offsetLeft},${postsContent.offsetTop})");
+    }*/
+
+    postsBg.children = [e];
+
+    var w = postsContent.clientWidth;
+    var h = postsContent.clientHeight;
+    var path =
+      "polygon("
+      "0px 8px,"
+      "8px 0px,"
+      "${w - 8}px 0px,"
+      "${w}px 8px,"
+      "${w}px ${h - 8}px,"
+      "${w - 8}px ${h}px,"
+      "8px ${h}px,"
+      "0px ${h - 8}px"
+      ")";
+    print(path);
+    postsContent.style.clipPath = path;
+  }
+
+  void render() {
+    var marg = "${max(8, min(128, max(0, ((body.clientWidth - 1170) / 2).floor())))}px";
+    content.style.marginTop = marg;
+    content.style.marginLeft = marg;
+    content.style.marginRight = marg;
+
+    renderHeader();
+    renderAbout();
+    renderLinks();
+    renderPosts();
+  }
+
+  ResizeObserver((entries, observer) {
+    render();
+  }).observe(querySelector("body"));
+}
